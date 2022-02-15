bd210e60af5910b0e056c243d7698aacd75a9dac
st0rmg0d <55540414+st0rmg0d@users.noreply.github.com>
2022-02-16 02:25:25 +0600

    Add files via upload

---
+++ src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -0,0 +1,75 @@
+package com.example.android.guesstheword.screens.game
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import com.example.android.guesstheword.R
+import com.example.android.guesstheword.databinding.GameFragmentBinding
+
+class GameFragment : Fragment() {
+
+    private lateinit var binding: GameFragmentBinding
+
+    private lateinit var viewModel: GameViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        binding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.game_fragment,
+                container,
+                false
+        )
+        Log.i("GameFragment", "Called ViewModelProvider.get")
+
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+
+        binding.correctButton.setOnClickListener { onCorrect() }
+        binding.skipButton.setOnClickListener { onSkip() }
+        binding.endGameButton.setOnClickListener { onEndGame() }
+        updateScoreText()
+        updateWordText()
+        return binding.root
+    }
+
+
+    private fun onSkip() {
+        viewModel.onSkip()
+        updateWordText()
+        updateScoreText()
+    }
+    private fun onCorrect() {
+        viewModel.onCorrect()
+        updateScoreText()
+        updateWordText()
+    }
+
+    private fun onEndGame() {
+        gameFinished()
+    }
+
+
+    private fun updateWordText() {
+        binding.wordText.text = viewModel.word
+
+    }
+
+    private fun updateScoreText() {
+        binding.scoreText.text = viewModel.score.toString()
+    }
+
+    private fun gameFinished() {
+        Toast.makeText(activity, "Game has just finished", Toast.LENGTH_SHORT).show()
+        val action = GameFragmentDirections.actionGameToScore()
+        action.score = viewModel.score
+        NavHostFragment.findNavController(this).navigate(action)
+    }
+}
---
+++ src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -0,0 +1,41 @@
+package com.example.android.guesstheword.screens.score
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
+import com.example.android.guesstheword.R
+import com.example.android.guesstheword.databinding.ScoreFragmentBinding
+
+
+class ScoreFragment : Fragment() {
+
+    private lateinit var viewModel: ScoreViewModel
+    private lateinit var viewModelFactory: ScoreViewModelFactory
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        val binding: ScoreFragmentBinding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.score_fragment,
+                container,
+                false
+        )
+
+        viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(arguments!!).score)
+        viewModel = ViewModelProvider(this, viewModelFactory)
+                .get(ScoreViewModel::class.java)
+
+        binding.scoreText.text = viewModel.score.toString()
+
+        return binding.root
+    }
+}
